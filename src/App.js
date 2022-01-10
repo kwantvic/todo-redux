34839257a5dc3c2e54041a47bb7a5000b6069b8c
kwant <user@macBookAir-kwant.local>
2022-01-10 17:18:10 +0200

    todoRedux_v4.0: add redux in project + react-redux

--- src/App.js
+++ src/App.js
@@ -2,63 +2,11 @@ import React from 'react';
 import { Paper, Divider, Button, List, Tabs, Tab } from '@mui/material';
 import { AddField } from './components/AddField';
 import { Item } from './components/Item';
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'ADD_TASK': {
-      return [
-        ...state,
-        {
-          id: state.length ? state[state.length - 1].id + 1 : 1,
-          text: action.payload.text,
-          completed: action.payload.completed,
-        },
-      ];
-    }
-    case 'REMOVE_TASK': {
-      return state.filter((item) => item.id !== action.payload.id);
-    }
-    case 'EDIT_TASK': {
-      return state.map((obj) =>
-        obj.id === action.payload.id
-          ? {
-              ...obj,
-              text: action.payload.text,
-            }
-          : obj,
-      );
-    }
-    case 'TOGGLE_COMPLETED': {
-      return state.map((obj) =>
-        obj.id === action.payload.id
-          ? {
-              ...obj,
-              completed: !obj.completed,
-            }
-          : obj,
-      );
-    }
-    case 'REMOVE_ALL': {
-      return [];
-    }
-    case 'SELECT_ALL': {
-      return state.map((obj) => ({
-        ...obj,
-        completed: true,
-      }));
-    }
-    case 'DESELECT_ALL': {
-      return state.map((obj) => ({
-        ...obj,
-        completed: false,
-      }));
-    }
-  }
-  return state;
-}
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
-  const [state, dispatch] = React.useReducer(reducer, []);
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
   const [isToggleBtn, setToggleBtn] = React.useState(true);
   const [activeItem, setActiveItem] = React.useState(0);
